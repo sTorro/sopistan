b81adebc01bfe3b6946fd7faaca8459df4d73d35
adesanto asman <adesantoasman@gmail.com>
2016-08-28 21:48:57 +0700

    Set screen size

--- desktop/src/com/torrosoft/sopistan/desktop/DesktopLauncher.java
+++ desktop/src/com/torrosoft/sopistan/desktop/DesktopLauncher.java
@@ -1,7 +1,7 @@
 /*
  * File name: DesktopLauncher.java
  * This file is part of Sopistan project.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 3 of the License, or (at your
@@ -9,10 +9,10 @@
  * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with triviazo-project.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Copyright 2016 Sergio Torró.
  */
 
@@ -26,6 +26,9 @@ import com.torrosoft.sopistan.core.Sopistan;
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 800;
+        config.height = 600;
+
 		new LwjglApplication(new SopistanMain(), config);
 
 		// quick test
